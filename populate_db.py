f93cdd7c63dc83bd9aa8065684ed2c15109872d8
Muiz Rusli <2783261r@student.gla.ac.uk>
2024-03-14 21:43:41 +0000

    expanded populate_db to add more and add user

--- populate_db.py
+++ populate_db.py
@@ -7,6 +7,7 @@ django.setup()
 
 from django.utils import timezone
 from formula.models import *
+from random import randint
 
 # GENERIC FIELDS
 GENERIC_DESC = "This object is created as part of populate_db.py testing. This description is generic and is the same for all object. Object should be deleted before deployment."
@@ -34,11 +35,11 @@ def add_post(title, description, content, file, viewership, date_added, topic, a
     pass
 
 def add_user(username, email, password, student_id, first_name, last_name, bio, admin):
-    u = User.objects.get_or_create(username=username)
+    u = User.objects.get_or_create(username=username)[0]
     u.email = email
     u.set_password(password)
     u.save()
-    v = UserProfile.objects.get_or_create(user_username=username)
+    v = UserProfile.objects.get_or_create(user=u)[0]
     v.student_id = student_id
     v.first_name = first_name
     v.last_name = last_name
@@ -49,53 +50,190 @@ def add_user(username, email, password, student_id, first_name, last_name, bio, 
 
 
 def create_objects():
-
-    # Category - software
+    # Category - Software
     software = [
         {
-            'name':'Java',
-            'description':GENERIC_DESC,
-            'date_added':timezone.now(),
+            'name': 'Java',
+            'description': GENERIC_DESC,
+            'date_added': timezone.now(),
         },
         {
-            'name':'Python',
-            'description':GENERIC_DESC,
-            'date_added':timezone.now(),
+            'name': 'Python',
+            'description': GENERIC_DESC,
+            'date_added': timezone.now(),
         },
         {
-            'name':'C++',
-            'description':GENERIC_DESC,
-            'date_added':timezone.now(),
+            'name': 'C++',
+            'description': GENERIC_DESC,
+            'date_added': timezone.now(),
         }
     ]
 
     # Category - PR
-    public = [
+    public_relations = [
         {
-            'name':'Finance',
-            'description':GENERIC_DESC,
-            'date_added':timezone.now(),
+            'name': 'Finance',
+            'description': GENERIC_DESC,
+            'date_added': timezone.now(),
         },
         {
-            'name':'Marketing',
-            'description':GENERIC_DESC,
-            'date_added':timezone.now(),
+            'name': 'Marketing',
+            'description': GENERIC_DESC,
+            'date_added': timezone.now(),
         },
     ]
-    
-    categories_dict = {'Software':{'topics':software,
-                                   'description':GENERIC_DESC,
-                                   'date_added':timezone.now(),
-                                   'parent':Category.EVEHICLE,
-                                   },
-                        'Public':{'topics':public,
-                                  'description':GENERIC_DESC,
-                                  'date_added':timezone.now(),
-                                  'parent':Category.OPERATION
-                                  },
-                    }
-    
+
+    # Category - Engineering Projects
+    engineering_projects = [
+        {
+            'name': 'Aerodynamics',
+            'description': GENERIC_DESC,
+            'date_added': timezone.now(),
+        },
+        {
+            'name': 'Chassis Design',
+            'description': GENERIC_DESC,
+            'date_added': timezone.now(),
+        },
+        {
+            'name': 'Powertrain',
+            'description': GENERIC_DESC,
+            'date_added': timezone.now(),
+        },
+        {
+            'name': 'Suspension',
+            'description': GENERIC_DESC,
+            'date_added': timezone.now(),
+        },
+        {
+            'name': 'Electronics',
+            'description': GENERIC_DESC,
+            'date_added': timezone.now(),
+        },
+        {
+            'name': 'Materials',
+            'description': GENERIC_DESC,
+            'date_added': timezone.now(),
+        },
+        {
+            'name': 'Testing and Simulation',
+            'description': GENERIC_DESC,
+            'date_added': timezone.now(),
+        },
+    ]
+
+    # Category - Design
+    design = [
+        {
+            'name': 'Vehicle Design',
+            'description': GENERIC_DESC,
+            'date_added': timezone.now(),
+        },
+        {
+            'name': 'Graphics Design',
+            'description': GENERIC_DESC,
+            'date_added': timezone.now(),
+        },
+    ]
+
+    # Category - Business Operations
+    business_operations = [
+        {
+            'name': 'Sponsorship',
+            'description': GENERIC_DESC,
+            'date_added': timezone.now(),
+        },
+        {
+            'name': 'Event Management',
+            'description': GENERIC_DESC,
+            'date_added': timezone.now(),
+        },
+    ]
+
+    categories_dict = {
+        'Software': {
+            'topics': software,
+            'description': GENERIC_DESC,
+            'date_added': timezone.now(),
+            'parent': Category.GENERAL,
+        },
+        'Public Relations': {
+            'topics': public_relations,
+            'description': GENERIC_DESC,
+            'date_added': timezone.now(),
+            'parent': Category.OPERATION,
+        },
+        'Engineering Projects': {
+            'topics': engineering_projects,
+            'description': GENERIC_DESC,
+            'date_added': timezone.now(),
+            'parent': Category.EVEHICLE,
+        },
+        'Design': {
+            'topics': design,
+            'description': GENERIC_DESC,
+            'date_added': timezone.now(),
+            'parent': Category.GENERAL,
+        },
+        'Business Operations': {
+            'topics': business_operations,
+            'description': GENERIC_DESC,
+            'date_added': timezone.now(),
+            'parent': Category.OPERATION,
+        },
+    }
+
     return categories_dict
+   
+
+
+def create_dummy_categories():
+    categories_dict = {}
+    
+    def generate_cd(cat_name, cat_parent):
+        categories_dict[cat_name] = {'description':GENERIC_DESC,
+                                     'date_added':timezone.now(),
+                                     'parent':cat_parent,}
+
+    generate_cd('Engineering Projects', Category.EVEHICLE)
+    generate_cd('Software Development', Category.EVEHICLE)
+    generate_cd('Design', Category.GENERAL)
+    generate_cd('Business Operations', Category.OPERATION)
+    generate_cd('Research and Development', Category.EVEHICLE)
+    generate_cd('Testing', Category.EVEHICLE)
+    generate_cd('Marketing', Category.OPERATION)
+    generate_cd('Finance', Category.OPERATION)
+    generate_cd('Events', Category.OPERATION)
+    generate_cd('Education', Category.GENERAL)
+    generate_cd('Community Outreach', Category.OPERATION)
+
+    return categories_dict
+
+
+def create_dummy_topics(categories_dict:dict):
+    for category in categories_dict:
+        topic_list = []
+
+    def dummy_topic(topic_name, category):
+        topic = {
+                    'name':topic_name,
+                    'description':GENERIC_DESC,
+                    'date_added':timezone.now(),
+                    'category':category,
+                }
+        return topic
+
+def test_add_user():
+    add_user(username='bobbykeren', 
+             email='bob@gmail.com', 
+             password=GENERIC_PASSWORD, 
+             student_id=2773789, 
+             first_name='Bob', 
+             last_name='Keren', 
+             bio=GENERIC_DESC, 
+             admin=False)
+    print(f'USER add succesful - Bob')
+
 
 
 def populate():
@@ -110,10 +248,11 @@ def populate():
     # Print out the categories we have added.
     for category in Category.objects.all():
         for topic in Topic.objects.filter(category=category):
-            print(f'Add Succesful - {category}: {topic}')
+            print(f'CATEGORY:TOPIC add succesful - {category}: {topic}')
 
 
 # Start execution here!
 if __name__ == '__main__':
     print('Starting Formula1 population script...')
-    populate()
+    #populate()
+    test_add_user()
