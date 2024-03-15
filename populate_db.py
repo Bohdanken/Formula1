1fefea648ab3f3cdc9beebe5e1343848bf4a2af6
Muiz Rusli <2783261r@student.gla.ac.uk>
2024-03-15 03:02:26 +0000

    extended populate_db for category,topic,profiles and added db_dummy for dummy values in database

--- populate_db.py
+++ populate_db.py
@@ -7,7 +7,7 @@ django.setup()
 
 from django.utils import timezone
 from formula.models import *
-from random import randint
+from db_dummy import *
 
 # GENERIC FIELDS
 GENERIC_DESC = "This object is created as part of populate_db.py testing. This description is generic and is the same for all object. Object should be deleted before deployment."
@@ -23,10 +23,15 @@ def add_category(name, description, date_added, parent):
     return c
 
 def add_topic(name, description, date_added, category):
-    t = Topic.objects.get_or_create(category=category, name=name)[0]
+    try:
+        cat = Category.objects.get(name=category)
+    except Category.DoesNotExist:
+        print(f'Category {category} does not exist for TOPIC: {name}.')
+        return None
+    t = Topic.objects.get_or_create(category=cat, name=name)[0]
     t.description = description
     t.date_added = date_added
-    t.category = category
+    t.category = cat
     t.save()
     return t
 
@@ -34,20 +39,38 @@ def add_post(title, description, content, file, viewership, date_added, topic, a
     p = Post.objects.get_or_create(id=id)[0]
     pass
 
-def add_user(username, email, password, student_id, first_name, last_name, bio, admin):
-    u = User.objects.get_or_create(username=username)[0]
-    u.email = email
-    u.set_password(password)
-    u.save()
-    v = UserProfile.objects.get_or_create(user=u)[0]
-    v.student_id = student_id
-    v.first_name = first_name
-    v.last_name = last_name
-    v.bio = bio
-    v.admin = admin
-    v.save()
-    return v
+def add_user(username, email, password, student_id, first_name, last_name, bio, is_admin):
+    user = add_user_only(username=username, email=email, password=password, first_name=first_name, last_name=last_name)
+    profile = add_user_profile(username=username, student_id=student_id, bio=bio, is_admin=is_admin)
+    return profile
 
+def add_user_only(username, email, password, first_name, last_name):
+    user = User.objects.get_or_create(username=username)[0]
+    user.email = email
+    user.set_password(password)
+    user.first_name = first_name
+    user.last_name = last_name
+    user.save()
+    return user
+
+def add_user_profile(username, student_id, bio, is_admin):
+    try:
+        user = User.objects.get(username=username)
+        user_p = UserProfile.objects.get_or_create(user=user)
+        if user_p[1] is False:
+            print(f'User {username} already added')
+            return user_p[0]
+        else:
+            user_p = user_p[0]
+        user_p.student_id = student_id
+        user_p.bio = bio
+        user_p.is_admin = is_admin
+        user_p.save()
+        return user_p
+    
+    except User.DoesNotExist:
+        print(f'USER {username} is not found in the database.')
+        return None
 
 def create_objects():
     # Category - Software
@@ -191,48 +214,89 @@ def create_dummy_categories():
     categories_dict = {}
     
     def generate_cd(cat_name, cat_parent):
-        categories_dict[cat_name] = {'description':GENERIC_DESC,
+        categories_dict[cat_name] = {'name':cat_name,
+                                     'description':GENERIC_DESC,
                                      'date_added':timezone.now(),
-                                     'parent':cat_parent,}
+                                     'parent':cat_parent,
+                                     }
 
-    generate_cd('Engineering Projects', Category.EVEHICLE)
-    generate_cd('Software Development', Category.EVEHICLE)
-    generate_cd('Design', Category.GENERAL)
-    generate_cd('Business Operations', Category.OPERATION)
-    generate_cd('Research and Development', Category.EVEHICLE)
-    generate_cd('Testing', Category.EVEHICLE)
-    generate_cd('Marketing', Category.OPERATION)
-    generate_cd('Finance', Category.OPERATION)
-    generate_cd('Events', Category.OPERATION)
-    generate_cd('Education', Category.GENERAL)
-    generate_cd('Community Outreach', Category.OPERATION)
+    for category, parent in CategoryDummy.CATEGORIES:
+        generate_cd(category, parent)
 
     return categories_dict
 
 
-def create_dummy_topics(categories_dict:dict):
-    for category in categories_dict:
-        topic_list = []
+def create_dummy_topics():
+    topics_dict = {}
 
-    def dummy_topic(topic_name, category):
-        topic = {
-                    'name':topic_name,
-                    'description':GENERIC_DESC,
-                    'date_added':timezone.now(),
-                    'category':category,
-                }
-        return topic
+    def generate_td(topic_name, category):
+        topics_dict[topic_name] = { 'name':topic_name,
+                                    'description':GENERIC_DESC,
+                                    'date_added':timezone.now(),
+                                    'category':category,
+                                    }
+        
+    for topic, category in TopicDummy.TOPICS:
+        generate_td(topic, category)
+        
+    return topics_dict
+
+
+def assign_topics_to_categories(categories_dict:dict, topics_dict:dict):
+    for topic in topics_dict:
+        category = topics_dict[topic]['categories']
+        if not 'topics' in categories_dict[category]:
+            categories_dict[category]['topics'] = []
+        categories_dict[category]['topics'].append(topics_dict[topic])
+    
+    return categories_dict
+
+
+def create_dummy_user():
+    for user in UserDummy.USERS:
+        username = user['username']
+        add_user(username=username,
+                 email=user['email'],
+                 password=GENERIC_PASSWORD,
+                 first_name=user['first_name'],
+                 last_name=user['last_name'],
+                 student_id=UserProfileDummy.USER_PROFILE[username]['student_id'],
+                 bio=GENERIC_DESC,
+                 is_admin=False
+                 )
+        print(f'PROFILE add succesful - {username}')
+
+
+def test_add_category():
+    test_category = add_category(name='Test Category',
+                 description=GENERIC_DESC,
+                 date_added=timezone.now(),
+                 parent=Category.GENERAL
+                 )
+    print(f'CATEGORY add succesful - Test Category')
+    return test_category
+
+def test_add_topic(category:Category):
+    test_topic = add_topic(name='Fake Topic',
+              description=GENERIC_DESC,
+              date_added=timezone.now(),
+              category=category
+              )
+    print(f'TOPIC add succesful - Fake Topic in {category.name}')
+    return test_topic
 
 def test_add_user():
-    add_user(username='bobbykeren', 
-             email='bob@gmail.com', 
+    test_user = add_user(username='normaluser', 
+             email='noadmin@privileges.com', 
              password=GENERIC_PASSWORD, 
-             student_id=2773789, 
-             first_name='Bob', 
-             last_name='Keren', 
-             bio=GENERIC_DESC, 
-             admin=False)
-    print(f'USER add succesful - Bob')
+             first_name='Normal', 
+             last_name='User', 
+             student_id=1111111, 
+             bio='This is a generic user with no privileges', 
+             is_admin=False
+             )
+    print(f'USER add succesful - normaluser')
+    return test_user
 
 
 
@@ -241,7 +305,7 @@ def populate():
 
     for cat, cat_data in cats.items():
         category = add_category(cat, cat_data['description'], cat_data['date_added'], cat_data['parent'])
-        for topic in cat_data['topics']:
+    for topic in cat_data['topics']:
             add_topic(topic['name'], topic['description'], topic['date_added'], category)
 
 
@@ -250,9 +314,23 @@ def populate():
         for topic in Topic.objects.filter(category=category):
             print(f'CATEGORY:TOPIC add succesful - {category}: {topic}')
 
+def dummy_populate():
+    categories_dict = create_dummy_categories()
+    for cat, cat_data in categories_dict.items():
+        added_category = add_category(cat_data['name'], cat_data['description'], cat_data['date_added'], cat_data['parent'])
+        print(f'CATEGORY add succesful - {added_category}')
+
+    topics_dict = create_dummy_topics()
+    for topic, top_data in topics_dict.items():
+        added_topic = add_topic(top_data['name'], top_data['description'], top_data['date_added'], top_data['category'])
+        print(f'TOPIC add succesful - {added_topic} in {added_topic.category}')
+
+    profiles_dict = create_dummy_user()
 
 # Start execution here!
 if __name__ == '__main__':
     print('Starting Formula1 population script...')
-    #populate()
-    test_add_user()
+    t_user = test_add_user()
+    t_category = test_add_category()
+    t_topic = test_add_topic(t_category)
+    dummy_populate()
