237f087edc932579ae0d0158b2804cc7a81798b8
Bohdan Shevchenko <104861557+Bohdanken@users.noreply.github.com>
2024-03-17 13:48:04 +0000

    Untrack __pycache__ directories and apply .gitignore (#14)

--- Formula1/settings.py
+++ Formula1/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django',
     'formula',
-    'registration',
+    'registration'
 ]
 
 PASSWORD_HASHERS = (
