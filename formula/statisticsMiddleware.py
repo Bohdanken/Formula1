a3cafbfcddf5bf64a5b9424f1ee40577d559d24d
“Luke <2767178c@student.gla.ac.uk>
2024-03-20 17:45:21 +0000

    feature-posts_and_topic_counts_show

--- formula/statisticsMiddleware.py
+++ formula/statisticsMiddleware.py
@@ -1,20 +1,22 @@
 
 from django.utils.deprecation import MiddlewareMixin
 from formula.models import Post, Topic, CustomUser
-from django.shortcuts import render
 
 class StatisticsMiddleware(MiddlewareMixin):
-    def statistics_processor(request):
-        total_posts = Post.objects.count()
-        total_topics = Topic.objects.count()
-        total_members = CustomUser.objects.count()
-        newest_member = CustomUser.objects.latest('date_joined')
+    def process_request(self, request):
+        
+        total_posts = Post.objects.all().count()
+        total_topics = Topic.objects.all().count()
+        #total_members = CustomUser.objects.count()
+        #newest_member = CustomUser.objects.latest('date_joined')
+        # Attach the statistics to the request object
+        request.total_posts = total_posts
+        request.total_topics = total_topics
+        #request.total_members = total_members
+        #request.newest_member = newest_member
 
-        context_dict = {
-            'total_posts': total_posts,
-            'total_topics': total_topics,
-            'total_members': total_members,
-            'newest_member': newest_member.get_full_name()  
-        }
-        return render(request,'formula/base.html', context=context_dict)
+        
+    
+
+
     
