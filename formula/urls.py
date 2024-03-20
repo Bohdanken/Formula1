9f9bd323203c463aed4c44b2096321f27cd9d788
“Luke <2767178c@student.gla.ac.uk>
2024-03-20 19:02:36 +0000

    Basic unstyled profile

--- formula/urls.py
+++ formula/urls.py
@@ -6,7 +6,8 @@ app_name = 'formula'
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-    path('profile/', views.show_profile, name='profile'),
+    #path('profile/', views.show_profile, name='profile'),
+    path('profile/<str:username>/', views.show_profile, name='profile'),
     path('team/<slug:team_slug>/', views.show_team, name='team'),
     path('test/', views.testLogoutView),
     #path('search/', views.search_results, name='search'),
